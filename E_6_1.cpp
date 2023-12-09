7ecdfa535e8c22c79ce8ad25799fbf2ed920c985
BiancaStroppiana <biancastroppiana02@gmail.com>
2023-12-09 15:25:32 +0100

    last modifications

--- E_6_1.cpp
+++ E_6_1.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 using namespace std;
 
+//questo programma stampa un array di 30 numeri randomici compresi tra 0 e 99
+
 int main () {
     // Definizione dell'array
     int dim=30;
@@ -10,16 +12,16 @@ int main () {
    
     //numeri tra 0 e 99
     srand(time(NULL));
-    for (int i=0; i<dim; i++) {      //int i=0; i != '\0'; i++
-        num[i]= rand()%100;         //perche %100?
+    for (int i=0; i<dim; i++) {      
+        num[i]= rand()%100;         //perche %100? perche il resto di un numero diviso 100 è sicuramente compreso tra 0 e 99, altrimenti si potrebbe dividere ancora per 100
     }
     
     // Stampa dell'array
-    for (int j=0; j<dim-1; j++ ) {
+    for (int j=0; j<dim; j++ ) {
         cout<<num[j];
         cout<<" ";
     }
-    cout<<num[dim-1]<<endl;
+    
 return 0;
 
 }
